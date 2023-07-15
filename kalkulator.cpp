c1f892e0e9b7808495984ca639e0560eac30d1f5
Daniel Ryukensi <70358451+ryukensi87@users.noreply.github.com>
2023-07-15 13:20:15 +0700

    Add files via upload

---
+++ kalkulator.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	float a,b, hasil;
+	char aritmatika;
+
+	cout << "Selamat datang di program kalkulator sederhana \n \n";
+
+	//masukkan input dari user
+	cout << "Masukkan nilai pertama: ";
+	cin >> a;
+	cout << "Pilih operator +,-,/,*: ";
+	cin >> aritmatika;
+	cout << "Masukkan nilai kedua: ";
+	cin >> b;
+
+	cout << "\nHasil perhitungan: ";
+	cout << a << aritmatika << b;
+
+	if (aritmatika == '+'){
+	hasil = a + b;
+	} else if (aritmatika == '-'){
+	hasil = a - b;
+	} else if (aritmatika == '/'){
+	hasil = a / b;
+	} else if (aritmatika == '*'){
+	hasil = a * b;
+	} else {
+		cout <<"operator anda salah" << endl;
+	}
+
+	
+	cout << " = " << hasil << endl;
+
+	cin.get();
+	return 0;
+
+}
